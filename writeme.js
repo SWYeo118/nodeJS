267d2831b3133b82a9f87c320984aab1b0a4ab13
여상우 <effortmeta@gmail.com>
2023-11-14 21:04:38 +0900

    readme filesystem 연습2

--- writeme.js
+++ writeme.js
@@ -1,9 +1,8 @@
 const fs = require('fs').promises; // fs 모듈은 promise를 지원하는데, 이걸 promises로 하면 됨!
 
-fs.readFile('./readme.txt')
-    .then((data) => {
-            console.log(data);
-            console.log(data.toString());
+fs.writeFile('./writeme.txt', '글자가 입력됩니다.')
+    .then(() => {
+
     })
     .catch((error) => {
         throw error;
