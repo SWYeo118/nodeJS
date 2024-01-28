c8fe015cf58d646161ceda655522632607d14d8d
여상우 <effortmeta@gmail.com>
2024-01-28 22:38:38 +0900

    아유 귀찮아

--- writeme.js
+++ writeme.js
@@ -11,4 +11,9 @@ fs.writeFile('./writeme.txt', '글자가 입력됩니다.')
         throw error;
     });
 
-// fs로 파일시스템 모듈을 불러온다.
+/* fs로 파일시스템 모듈을 불러온다.
+writeFile로 해당 데이터를 입력한다.
+그리고 then으로 해당 파일을 writeme.txt 라는 파일에 쓴다.
+그리고 data를 콘솔로그로 출력한다. js.writeFile의 데이터 기본값이 'data'인듯
+마지막 .catch로 에러 잡아서 출력
+ */
